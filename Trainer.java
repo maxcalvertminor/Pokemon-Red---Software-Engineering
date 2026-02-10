25fd3c8dc756442f91f46869419df951be2dd937
Max Calvert-Minor <maxincalvertminor@gmail.com>
2026-02-10 07:56:01 -0600

    Add files via upload
    Each Trainer has an active Pokemon, use void SendOut() to change active Pokemon. Eventually the String[] Dialogue will be the dialogue before and after battle.

---
+++ Trainer.java
@@ -0,0 +1,11 @@
+public class Trainer {
+    public Pokemon[] team;
+    public String name;
+    public String[] dialogue;
+
+    public Trainer(String n, Pokemon[] t = null) {
+        name = n;
+        team = t;
+        
+    } 
+}
