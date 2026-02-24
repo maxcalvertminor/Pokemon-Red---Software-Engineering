9b3009594e164bb5c151d0f28096b179867ed643
Max Calvert-Minor <maxincalvertminor@gmail.com>
2026-02-24 08:31:40 -0600

    Refactor HelperMethods to include type conversion
    Added ConvertStringTypeToTypeID method for type conversion.

--- HelperMethods.java
+++ HelperMethods.java
@@ -25,4 +25,48 @@ public class HelperMethods {
         //Normal(0), Fire(1), Water(2), Grass(3), Electric(4), Ice(5), Fighting(6), Poison(7), Ground(8), Flying(9), Psychic(10), Bug(11), Rock(12), Ghost(13), Dragon(14), Dark(15), Steel(16), and Fairy(17)
         return typeEffectiveness[type1][type2];
     }
+
+    public static int ConvertStringTypeToTypeID(String type) {
+        switch(type) {
+            case "Normal":
+                return 0;
+            case "Fire":
+                return 1;
+            case "Water":
+                return 2;
+            case "Grass":
+                return 3;
+            case "Electric":
+                return 4;
+            case "Ice":
+                return 5;
+            case "Fighting":
+                return 6;
+            case "Poison":
+                return 7;
+            case "Ground":
+                return 8;
+            case "Flying":
+                return 9;
+            case "Psychic":
+                return 10;
+            case "Bug":
+                return 11;
+            case "Rock":
+                return 12;
+            case "Ghost":
+                return 13;
+            case "Dragon":
+                return 14;
+            case "Dark":
+                return 15;
+            case "Steel":
+                return 16;
+            case "Fairy":
+                return 17;
+            default:
+                return -1;
+        }
+    }
+
 }
